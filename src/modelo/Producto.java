ac57e12f0d03e4fe4ea6ab7b8cb2d4e2d10e2a12
Eidikey <alexneithor@gmail.com>
2026-03-25 19:14:14 -0600

    Abstract Prueba

--- src/modelo/Producto.java
+++ src/modelo/Producto.java
@@ -1,17 +1,77 @@
-abstract class Producto {
+package modelo;
 
-  private int id;
-  private String nombre;
-  private float precioBase;
-  private int cantidadDisponible;
+public abstract class Producto {
+      protected String id;
+      protected String nombre;
+      protected double precioBase;
+      protected int cantidadDisponible;
 
-  double calcularPrecioFinal() {
-    return precioBase;
+      public Producto(String id, String nombre, double precioBase, int cantidadDisponible) {
+            this.id = id;
+            this.nombre = nombre;
+            setPrecioBase(precioBase);
+            setCantidadDisponible(cantidadDisponible);
+        }
+
+      // Métodos abstractos que obligan a las subclases a implementar su lógica
+  //     public abstract double calcularPrecioFinal();
+  //         public abstract void mostrarInformacion();
+  //
+  //             // Getters y Setters con validaciones (Encapsulamiento)
+  //                 public String getId() {
+  //                         return id;
+  //                             }
+  //
+  //                                 public void setId(String id) {
+  //                                         this.id = id;
+  //                                             }
+  //
+  //                                                 public String getNombre() {
+  //                                                         return nombre;
+  //                                                             }
+  //
+  //                                                                 public void setNombre(String nombre) {
+  //                                                                         this.nombre = nombre;
+  //                                                                             }
+  //
+  //                                                                                 public double getPrecioBase() {
+  //                                                                                         return precioBase;
+  //                                                                                             }
+  //
+  //                                                                                                 public void setPrecioBase(double precioBase) {
+  //                                                                                                         if (precioBase >= 0) {
+  //                                                                                                                     this.precioBase = precioBase;
+  //                                                                                                                             } else {
+  //                                                                                                                                         System.err.println("Error: El precio base no puede ser negativo.");
+  //                                                                                                                                                     this.precioBase = 0;
+  //                                                                                                                                                             }
+  //                                                                                                                                                                 }
+  //
+  //                                                                                                                                                                     public int getCantidadDisponible() {
+  //                                                                                                                                                                             return cantidadDisponible;
+  //                                                                                                                                                                                 }
+  //
+  //                                                                                                                                                                                     public void setCantidadDisponible(int cantidadDisponible) {
+  //                                                                                                                                                                                             if (cantidadDisponible >= 0) {
+  //                                                                                                                                                                                                         this.cantidadDisponible = cantidadDisponible;
+  //                                                                                                                                                                                                                 } else {
+  //                                                                                                                                                                                                                             System.err.println("Error: La cantidad no puede ser negativa.");
+  //                                                                                                                                                                                                                                         this.cantidadDisponible = 0;
+  //                                                                                                                                                                                                                                                 }
+  //                                                                                                                                                                                                                                                     }
+  //                                                                                                                                                                                                                                                     }
+  //
+  //                                                                                                                                                                                                                 }
+  //                                                                                                                                                                                             }
+  //                                                                                                                                                                                     }
+  //                                                                                                                                                                     }
+  //                                                                                                                             }
+  //                                                                                                         }
+  //                                                                                                 }
+  //                                                                                 }
+  //                                                                 }
+  //                                                 }
+  //                                 }
+  //                 }
   }
-
-  void mostrarInformacion() {
-    System.out.println("ID del producto: " + id + "Nombre del Producto: " + nombre + "Precio base: " + precioBase
-        + "Cantidad Disponible: " + cantidadDisponible);
-  }
-
 }
