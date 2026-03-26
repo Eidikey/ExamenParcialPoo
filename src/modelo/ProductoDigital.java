3768f04b5925b04a92a4498a63aba89e5bbc9aab
Alex <alexneithor@gmail.com>
2026-03-25 19:42:51 -0600

    Clase Producto Fisico

--- src/modelo/ProductoDigital.java
+++ src/modelo/ProductoDigital.java
@@ -1,9 +1,54 @@
+package modelo;
+
 public class ProductoDigital extends Producto {
   private double tamanoMB;
   private double porcentajeLicencia;
 
+  public ProductoDigital(String id, String nombre, double precioBase, int cantidadDisponible, double tamanoMB,
+      double porcentajeLicencia) {
+    super(id, nombre, precioBase, cantidadDisponible);
+    setTamanoMB(tamanoMB);
+    this.porcentajeLicencia = porcentajeLicencia;
+  }
+
   @Override
-  double calcularPrecioFinal() {
-    return super.calcularPrecioFinal() + porcentajeLicencia;
+  public double calcularPrecioFinal() {
+    return precioBase + porcentajeLicencia;
+  }
+
+  @Override
+  public void mostrarInformacion() {
+    System.out.println("TIPO: Producto Fisico");
+    System.out.println("ID: " + id);
+    System.out.println("Nombre: " + nombre);
+    System.out.println("Stock: " + cantidadDisponible);
+    System.out.println("Peso: " + tamanoMB + "MB");
+    System.out.println("POrcentaje Licencia: $" + porcentajeLicencia);
+    System.out.println("Precio Final: $" + calcularPrecioFinal());
+    System.out.println("---------------------------");
+  }
+
+  public double getTamanoMB() {
+    return tamanoMB;
+  }
+
+  public void setTamanoMB(double tamanoMB) {
+    if (tamanoMB >= 0) {
+      this.tamanoMB = tamanoMB;
+    } else {
+      this.tamanoMB = 0;
+    }
+  }
+
+  public double getPorcentajeLicencia() {
+    return porcentajeLicencia;
+  }
+
+  public void setPorcentajeEnvio(double porcentajeLicencia) {
+    if (porcentajeLicencia >= 0) {
+      this.porcentajeLicencia = porcentajeLicencia;
+    } else {
+      this.porcentajeLicencia = 0;
+    }
   }
 }
