82dd2992fbc4bc3c750c0cab68a96fef381a7556
Alex <alexneithor@gmail.com>
2026-03-25 19:26:00 -0600

    Clase Producto Digital

--- src/modelo/ProductoFisico.java
+++ src/modelo/ProductoFisico.java
@@ -1,9 +1,54 @@
+package modelo;
+
 public class ProductoFisico extends Producto {
   private double peso;
   private double costoEnvio;
 
+  public ProductoFisico(String id, String nombre, double precioBase, int cantidadDisponible, double peso,
+      double costoEnvio) {
+    super(id, nombre, precioBase, cantidadDisponible);
+    setPeso(peso);
+    setCostoEnvio(costoEnvio);
+  }
+
   @Override
-  double calcularPrecioFinal() {
-    return super.calcularPrecioFinal() + costoEnvio;
+  public double calcularPrecioFinal() {
+    return precioBase + costoEnvio;
+  }
+
+  @Override
+  public void mostrarInformacion() {
+    System.out.println("TIPO: Producto Fisico");
+    System.out.println("ID: " + id);
+    System.out.println("Nombre: " + nombre);
+    System.out.println("Stock: " + cantidadDisponible);
+    System.out.println("Peso: " + peso + "kg");
+    System.out.println("Costo Envio: $" + costoEnvio);
+    System.out.println("Precio Final: $" + calcularPrecioFinal());
+    System.out.println("---------------------------");
+  }
+
+  public double getPeso() {
+    return peso;
+  }
+
+  public void setPeso(double peso) {
+    if (peso >= 0) {
+      this.peso = peso;
+    } else {
+      this.peso = 0;
+    }
+  }
+
+  public double getCostoEnvio() {
+    return costoEnvio;
+  }
+
+  public void setCostoEnvio(double costoEnvio) {
+    if (costoEnvio >= 0) {
+      this.costoEnvio = costoEnvio;
+    } else {
+      this.costoEnvio = 0;
+    }
   }
 }
