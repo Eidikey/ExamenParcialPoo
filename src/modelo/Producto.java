1035a9504e19430415a9cb7daf2d48abfd5ee93c
Alex <alexneithor@gmail.com>
2026-03-25 19:21:44 -0600

    Clase Abstracta Producto

--- src/modelo/Producto.java
+++ src/modelo/Producto.java
@@ -1,77 +1,62 @@
 package modelo;
 
 public abstract class Producto {
-      protected String id;
-      protected String nombre;
-      protected double precioBase;
-      protected int cantidadDisponible;
+  protected String id;
+  protected String nombre;
+  protected double precioBase;
+  protected int cantidadDisponible;
 
-      public Producto(String id, String nombre, double precioBase, int cantidadDisponible) {
-            this.id = id;
-            this.nombre = nombre;
-            setPrecioBase(precioBase);
-            setCantidadDisponible(cantidadDisponible);
-        }
+  public Producto(String id, String nombre, double precioBase, int cantidadDisponible) {
+    this.id = id;
+    this.nombre = nombre;
+    setPrecioBase(precioBase);
+    setCantidadDisponible(cantidadDisponible);
+  }
 
-      // Métodos abstractos que obligan a las subclases a implementar su lógica
-  //     public abstract double calcularPrecioFinal();
-  //         public abstract void mostrarInformacion();
-  //
-  //             // Getters y Setters con validaciones (Encapsulamiento)
-  //                 public String getId() {
-  //                         return id;
-  //                             }
-  //
-  //                                 public void setId(String id) {
-  //                                         this.id = id;
-  //                                             }
-  //
-  //                                                 public String getNombre() {
-  //                                                         return nombre;
-  //                                                             }
-  //
-  //                                                                 public void setNombre(String nombre) {
-  //                                                                         this.nombre = nombre;
-  //                                                                             }
-  //
-  //                                                                                 public double getPrecioBase() {
-  //                                                                                         return precioBase;
-  //                                                                                             }
-  //
-  //                                                                                                 public void setPrecioBase(double precioBase) {
-  //                                                                                                         if (precioBase >= 0) {
-  //                                                                                                                     this.precioBase = precioBase;
-  //                                                                                                                             } else {
-  //                                                                                                                                         System.err.println("Error: El precio base no puede ser negativo.");
-  //                                                                                                                                                     this.precioBase = 0;
-  //                                                                                                                                                             }
-  //                                                                                                                                                                 }
-  //
-  //                                                                                                                                                                     public int getCantidadDisponible() {
-  //                                                                                                                                                                             return cantidadDisponible;
-  //                                                                                                                                                                                 }
-  //
-  //                                                                                                                                                                                     public void setCantidadDisponible(int cantidadDisponible) {
-  //                                                                                                                                                                                             if (cantidadDisponible >= 0) {
-  //                                                                                                                                                                                                         this.cantidadDisponible = cantidadDisponible;
-  //                                                                                                                                                                                                                 } else {
-  //                                                                                                                                                                                                                             System.err.println("Error: La cantidad no puede ser negativa.");
-  //                                                                                                                                                                                                                                         this.cantidadDisponible = 0;
-  //                                                                                                                                                                                                                                                 }
-  //                                                                                                                                                                                                                                                     }
-  //                                                                                                                                                                                                                                                     }
-  //
-  //                                                                                                                                                                                                                 }
-  //                                                                                                                                                                                             }
-  //                                                                                                                                                                                     }
-  //                                                                                                                                                                     }
-  //                                                                                                                             }
-  //                                                                                                         }
-  //                                                                                                 }
-  //                                                                                 }
-  //                                                                 }
-  //                                                 }
-  //                                 }
-  //                 }
+  public abstract double calcularPrecioFinal();
+
+  public abstract void mostrarInformacion();
+
+  // Getters y Setters con validaciones (Encapsulamiento)
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  public String getNombre() {
+    return nombre;
+  }
+
+  public void setNombre(String nombre) {
+    this.nombre = nombre;
+  }
+
+  public double getPrecioBase() {
+    return precioBase;
+  }
+
+  public void setPrecioBase(double precioBase) {
+    if (precioBase >= 0) {
+      this.precioBase = precioBase;
+    } else {
+      System.err.println("Error: El precio base no puede ser negativo.");
+      this.precioBase = 0;
+    }
+  }
+
+  public int getCantidadDisponible() {
+    return cantidadDisponible;
+  }
+
+  public void setCantidadDisponible(int cantidadDisponible) {
+    if (cantidadDisponible >= 0) {
+      this.cantidadDisponible = cantidadDisponible;
+    } else {
+      System.err.println("Error: La cantidad no puede ser negativa.");
+      this.cantidadDisponible = 0;
+    }
   }
 }
